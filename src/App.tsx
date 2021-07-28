2cb986faf347ca35e37a2e2ab279c5d70c1f16ad
bryanlopez <bryan.lopez@hunterdouglas.com>
2021-07-27 18:56:51 -0600

    homepage view

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
             <Router>
                 <Navigation />
                 <Switch>
-                    <Route path="/bryan-lopez" exact component={() => <Home />} />
+                    <Route path="/" exact component={() => <Home />} />
                     <Route path="/about" exact component={() => <About />} />
                     <Route path="/contact" exact component={() => <Contact />} />
                 </Switch>
